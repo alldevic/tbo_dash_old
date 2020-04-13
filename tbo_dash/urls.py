1fd53b97ce76af4e96231da9bf863b51f7d68c7b
Nikolay Bely <belyynikolay@gmail.com>
2020-04-13 18:38:55 +0700

    Add djoser

--- tbo_dash/urls.py
+++ tbo_dash/urls.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rest_framework.authtoken.views import obtain_auth_token 
 from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/schema/', get_schema_view(
         title="TBO dashboard",
-        description="API for all things …",
         version="1.0.0"
     ), name='docs-schema'),
-    path('auth/', obtain_auth_token, name='auth'),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
