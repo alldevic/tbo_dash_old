bf577fe2ac9c55f1d17d2ef6d7e40e4bfa83947f
Nikolay Bely <belyynikolay@gmail.com>
2020-04-13 21:00:55 +0700

    Add schema generation as file

--- tbo_dash/settings.py
+++ tbo_dash/settings.py
@@ -85,12 +85,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 25
 }
--- tbo_dash/urls.py
+++ tbo_dash/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/schema/', get_schema_view(
+    path('docs/schema.yaml', get_schema_view(
         title="TBO dashboard",
-        version="1.0.0"
+        version="1.0.0",
+        permission_classes=[permissions.IsAuthenticatedOrReadOnly]
     ), name='docs-schema'),
     path('auth/', include('djoser.urls.authtoken')),
 ]
