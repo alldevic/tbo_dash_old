fb8ae14ecbdd12a21031c5a97122815f860bcff3
Nikolay Bely <belyynikolay@gmail.com>
2020-05-24 04:24:15 +0700

    Add latency webclient, refactoring

--- tbo_dash/ws_main/socket_app.py
+++ tbo_dash/ws_main/socket_app.py
@@ -1,17 +1,17 @@
-import asyncio
 import socketio
 
 mgr = socketio.AsyncRedisManager('redis://redis:6379/0')
-sio_app = socketio.AsyncServer(async_mode='asgi', client_manager=mgr)
+sio_app = socketio.AsyncServer(async_mode='asgi',
+                               client_manager=mgr,
+                               ping_interval=3,
+                               cors_allowed_origins='*')
+
 
 @sio_app.event
 async def connect(sid, environ):
     print("connect ", sid)
 
+
 @sio_app.event
 async def disconnect(sid):
     print('disconnect ', sid)
-
-@sio_app.event
-async def ping_from_client(sid):
-    await sio_app.emit('pong_from_server', room=sid)
--- webclient/src/routes/index.svelte
+++ webclient/src/routes/index.svelte
@@ -1,79 +1,37 @@
 <script>
-  import { DataTable, Pagination } from "carbon-components-svelte";
+  import Table from "../components/Table.svelte";
+  import io from "socket.io-client";
 
-  let rows = [
-    {
-      id: "a",
-      name: "Load Balancer 3",
-      protocol: "HTTP",
-      port: 3000,
-      rule: "Round robin",
-      attached_groups: "Kevins VM Groups",
-      status: "Disabled"
-    },
-    {
-      id: "b",
-      name: "Load Balancer 1",
-      protocol: "HTTP",
-      port: 443,
-      rule: "Round robin",
-      attached_groups: "Maureens VM Groups",
-      status: "Starting"
-    },
-    {
-      id: "c",
-      name: "Load Balancer 2",
-      protocol: "HTTP",
-      port: 80,
-      rule: "DNS delegation",
-      attached_groups: "Andrews VM Groups",
-      status: "Active"
-    },
-    {
-      id: "d",
-      name: "Load Balancer 6",
-      protocol: "HTTP",
-      port: 3000,
-      rule: "Round robin",
-      attached_groups: "Marcs VM Groups",
-      status: "Disabled"
-    },
-    {
-      id: "e",
-      name: "Load Balancer 4",
-      protocol: "HTTP",
-      port: 443,
-      rule: "Round robin",
-      attached_groups: "Mels VM Groups",
-      status: "Starting"
-    },
-    {
-      id: "f",
-      name: "Load Balancer 5",
-      protocol: "HTTP",
-      port: 80,
-      rule: "DNS delegation",
-      attached_groups: "Ronjas VM Groups",
-      status: "Active"
-    }
+  let rows = [];
+  const headers = [
+    { key: "time", value: "Time" },
+    { key: "cid", value: "Client ID" },
+    { key: "latency", value: "Latency" }
   ];
-  let headers = [
-    { key: "name", value: "Name" },
-    { key: "protocol", value: "Protocol" },
-    { key: "port", value: "Port" },
-    { key: "rule", value: "Rule" },
-    { key: "attached_groups", value: "Attached Groups" },
-    { key: "status", value: "Status" }
-  ];
+
+  const socket = io("http://localhost:8000", { path: "/api/v1/wsdash" });
+
+  socket.on("connect", () => {
+    console.log("Connected, id=", socket.id);
+  });
+
+  let rowId = 0;
+
+  socket.on("pong", function(ms) {
+    rows = [
+      {
+        id: rowId++,
+        cid: socket.id,
+        latency: ms,
+        time: new Date(Date.now()).toLocaleString()
+      },
+      ...rows
+    ];
+  });
 </script>
-
-<style lang="scss" global>
-  @import "carbon-components/scss/components/pagination/pagination";
-  @import "carbon-components/scss/components/data-table/data-table";
-</style>
 
 <svelte:head>
   <title>TBO Dashboard</title>
 </svelte:head>
-<DataTable description="qweqe" title="qweqweqweqwee" {rows} {headers} />
-<Pagination totalItems={103} />
+
+<Table {rows} {headers} />
